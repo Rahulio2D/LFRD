a0a254797b22d8aac99d8b1fa481f64617a7f2b8
Rahul Sharma <rahulio2d@gmail.com>
2025-10-05 14:20:17 +0100

    Copy files from template folder

---
+++ helpers/copy.go
@@ -0,0 +1,51 @@
+package helpers
+
+import (
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+// CopyDir copies all files and subdirectories from sourceLocation to destinationLocation
+func CopyDir(sourceLocation string, destinationLocation string) error {
+	return filepath.WalkDir(sourceLocation, func(fileName string, fileEntry fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relPath, err := filepath.Rel(sourceLocation, fileName)
+		if err != nil {
+			return err
+		}
+
+		targetPath := filepath.Join(destinationLocation, relPath)
+
+		if fileEntry.IsDir() {
+			return os.MkdirAll(targetPath, 0755)
+		}
+
+		return CopyFile(fileName, targetPath)
+	})
+}
+
+func CopyFile(sourceLocation, destinationLocation string) error {
+	in, err := os.Open(sourceLocation)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(destinationLocation)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+
+	return out.Close()
+}
